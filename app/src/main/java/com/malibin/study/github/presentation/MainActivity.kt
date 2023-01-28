959478a7b878a1d4a29c73ec583fd8b5af96e5a8
malibinYun <nightmare73@naver.com>
2023-01-29 00:51:38 +0900

    Main ViewModel Factory 작성

--- app/src/main/java/com/malibin/study/github/presentation/MainActivity.kt
+++ app/src/main/java/com/malibin/study/github/presentation/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.malibin.study.github.presentation
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.malibin.study.github.R
+import com.malibin.study.github.data.repository.injector.DefaultGithubProfileRepositoryInjector
 
 class MainActivity : AppCompatActivity() {
+
+    private val mainViewModel: MainViewModel by viewModels { MainViewModelFactory(this) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+}
+
+class MainViewModelFactory(private val context: Context) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return when (modelClass) {
+            MainViewModel::class.java -> {
+                MainViewModel(
+                    DefaultGithubProfileRepositoryInjector
+                        .providesDefaultGithubProfileRepository(context)
+                )
+            }
+            else -> throw IllegalArgumentException("cannot find viewModelClass of $modelClass")
+        } as T
     }
 }
