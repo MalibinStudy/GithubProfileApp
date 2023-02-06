374f8f59d9ef2473b40de455444f32e5dc0ac269
stopkite <stopkite98@naver.com>
2023-02-07 02:58:51 +0900

    test: [GithubProfileRepository] 로컬에 유저 정보가 있음을 확인할 수 있다

--- app/src/test/java/com/malibin/study/github/data/repository/DefaultGithubProfileRepositoryTest.kt
+++ app/src/test/java/com/malibin/study/github/data/repository/DefaultGithubProfileRepositoryTest.kt
@@ -1,7 +1,49 @@
 package com.malibin.study.github.data.repository
 
-import org.junit.jupiter.api.Assertions.*
+import com.google.common.truth.Truth.assertThat
+import com.malibin.study.github.data.source.GithubProfileSource
+import com.malibin.study.github.domain.profile.GithubProfile
+import com.malibin.study.github.utils.InstantTaskExecutorExtension
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.RegisterExtension
 
 internal class DefaultGithubProfileRepositoryTest {
 
+    private lateinit var fakeLocalSource: GithubProfileSource
+    private lateinit var fakeRemoteSource: GithubProfileSource
+
+    companion object {
+        @JvmStatic
+        @RegisterExtension
+        val instantTaskExecutorExtension = InstantTaskExecutorExtension()
+    }
+
+    @BeforeEach
+    fun setUp() {
+        fakeLocalSource = mockk<GithubProfileSource>(relaxed = true)
+        fakeRemoteSource = mockk<GithubProfileSource>(relaxed = true)
+    }
+
+    @Test
+    fun `로컬에 유저 정보가 있음을 확인할 수 있다`() = runBlocking {
+        // given
+        val expectedSearch = fakeLocalSource.getGithubProfile("stopkite").isSuccess
+
+        coEvery {
+            fakeLocalSource.getGithubProfile("stopkite").isSuccess
+        } returns true
+
+        // when
+        val actualSearch = DefaultGithubProfileRepository(
+            fakeLocalSource,
+            fakeRemoteSource
+        ).getGithubProfile("stopkite").isSuccess
+
+        // then
+        assertThat(actualSearch).isEqualTo(expectedSearch)
+    }
 }
