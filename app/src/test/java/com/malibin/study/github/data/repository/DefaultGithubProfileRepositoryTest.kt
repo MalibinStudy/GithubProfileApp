8355baa2ce7cb09d2b17c5bdb0b8b23412e17026
murjune <sis9221302@gmail.com>
2023-02-13 17:40:34 +0900

    [test] Step2-1 GithubRepository Unit Test

--- app/src/test/java/com/malibin/study/github/data/repository/DefaultGithubProfileRepositoryTest.kt
+++ app/src/test/java/com/malibin/study/github/data/repository/DefaultGithubProfileRepositoryTest.kt
@@ -1,5 +1,143 @@
 package com.malibin.study.github.data.repository
 
+import com.google.common.truth.Truth.assertThat
+import com.malibin.study.github.data.source.GithubProfileSource
+import com.malibin.study.github.domain.profile.GithubProfile
+import com.malibin.study.github.domain.repository.GithubProfileRepository
+import com.malibin.study.github.utils.InstantTaskExecutorExtension
+import com.malibin.study.github.utils.MainCoroutineExtension
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.coVerifySequence
+import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.junit5.MockKExtension
+import io.mockk.slot
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.api.extension.RegisterExtension
 
-internal class DefaultGithubProfileRepositoryTest
+@OptIn(ExperimentalCoroutinesApi::class)
+@ExtendWith(MockKExtension::class)
+internal class DefaultGithubProfileRepositoryTest {
+
+    @RelaxedMockK
+    lateinit var localGithubProfileSource: GithubProfileSource
+
+    @RelaxedMockK
+    lateinit var remoteGithubProfileSource: GithubProfileSource
+    private lateinit var githubProfileRepository: GithubProfileRepository
+
+    @BeforeEach
+    fun setUp() {
+        githubProfileRepository = DefaultGithubProfileRepository(
+            localGithubProfileSource,
+            remoteGithubProfileSource
+        )
+    }
+
+    @Test
+    fun `githubProfile을 저장할 떄 내부 저장소에 저장된다`() = runTest {
+        // given
+        val profile = GithubProfile(
+            id = 1,
+            userName = "June",
+            avatarUrl = "",
+            name = "",
+            bio = "",
+            followersCount = 11,
+            followingCount = 12
+        )
+        // when
+        val actualResult = githubProfileRepository.saveGithubProfile(profile)
+        // then
+        assertAll(
+            { coVerify(exactly = 1) { localGithubProfileSource.saveGithubProfile(profile) } },
+            { assertThat(actualResult.isSuccess).isTrue() }
+        )
+    }
+
+    @Test
+    fun `local저장소에 userName에 해당하는 profile이 있으면 가져온다`() = runTest {
+        // given
+        val userName = "June"
+        val expectedProfile = GithubProfile(
+            id = 1,
+            userName = "June",
+            avatarUrl = "",
+            name = "",
+            bio = "",
+            followersCount = 11,
+            followingCount = 12
+        )
+        coEvery { githubProfileRepository.getGithubProfile(userName) } returns Result.success(
+            expectedProfile
+        )
+        // when
+        val actualResult: Result<GithubProfile> = githubProfileRepository.getGithubProfile(userName)
+        // then
+        assertAll(
+            { coVerify(exactly = 1) { localGithubProfileSource.getGithubProfile(userName) } },
+            { coVerify(exactly = 0) { remoteGithubProfileSource.getGithubProfile(userName) } },
+            { assertThat(actualResult.isSuccess).isTrue() }
+        )
+    }
+
+    @Test
+    fun `local저장소에 userName에 해당하는 profile이 없으면 remote에서 가져온다`() = runTest {
+        // given
+        val userName = "June"
+        val expectedException = IllegalArgumentException("$userName 이름에 해당하는 이름 없음")
+        val expectedProfile = GithubProfile(
+            id = 1,
+            userName = "June",
+            avatarUrl = "",
+            name = "",
+            bio = "",
+            followersCount = 11,
+            followingCount = 12
+        )
+        val expectedSlot = slot<GithubProfile>() // CapturingSlot 또는 MutableList로 인수를 캡처 할 수 있다.
+        coEvery { localGithubProfileSource.getGithubProfile(userName) } returns Result.failure(
+            expectedException
+        )
+        coEvery { remoteGithubProfileSource.getGithubProfile(userName) } returns Result.success(
+            expectedProfile
+        )
+        coEvery { localGithubProfileSource.saveGithubProfile(capture(expectedSlot)) } answers {
+            Result.success(
+                Unit
+            )
+        }
+        val actualResult = githubProfileRepository.getGithubProfile(userName)
+        // then
+        assertAll(
+            { coVerify(exactly = 1) { localGithubProfileSource.getGithubProfile(userName) } },
+            { coVerify(exactly = 1) { remoteGithubProfileSource.getGithubProfile(userName) } },
+            { coVerify(exactly = 1) { localGithubProfileSource.saveGithubProfile(expectedProfile) } },
+            {
+                coVerifySequence {
+                    localGithubProfileSource.getGithubProfile(userName)
+                    remoteGithubProfileSource.getGithubProfile(userName)
+                    localGithubProfileSource.saveGithubProfile(expectedProfile)
+                }
+            },
+            { assertThat(expectedSlot.captured).isEqualTo(expectedProfile) },
+            { assertThat(actualResult.getOrNull()).isEqualTo(expectedProfile) },
+            { assertThat(actualResult.isSuccess).isTrue() }
+        )
+    }
+
+    companion object {
+        @JvmField
+        @RegisterExtension
+        val instantTaskExecutorExtension = InstantTaskExecutorExtension()
+
+        @JvmField
+        @RegisterExtension
+        val coroutineExtension = MainCoroutineExtension()
+    }
+}
